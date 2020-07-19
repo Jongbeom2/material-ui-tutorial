08af824b94576d0c6cc8807f53f6f4dfa00ccee0
jongbeom <n113345@gmail.com>
2020-07-19 22:04:27 +0900

    배포 설정

---
+++ build/precache-manifest.bbbccdb1e35ba576fa9a606b170fc9d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94f6e090093fdd69258e361d252006b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb5a4e99f63f22ec6dc9",
+    "url": "/static/css/main.9bd3abb9.chunk.css"
+  },
+  {
+    "revision": "982392fb82148194ec3c",
+    "url": "/static/js/2.b3ac1a71.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.b3ac1a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5a4e99f63f22ec6dc9",
+    "url": "/static/js/main.147ebe22.chunk.js"
+  },
+  {
+    "revision": "81c4048fe1520eb70509",
+    "url": "/static/js/runtime-main.f01f0a6a.js"
+  }
+]);
